16585d96b57327419eaa9798c967f385af19b840
Nejra <nejra.pribinja@gmail.com>
2024-05-27 22:59:16 +0200

    social cards

--- components/ShareSection/ShareCards.tsx
+++ components/ShareSection/ShareCards.tsx
@@ -1,10 +1,21 @@
-import { SHARE_SOCIAL_1 } from "@/utils/content";
+import {
+  SHARE_SOCIAL_0,
+  SHARE_SOCIAL_1,
+  SHARE_SOCIAL_2,
+  SHARE_SOCIAL_3,
+  SHARE_SOCIAL_4,
+  SHARE_SOCIAL_5,
+} from "@/utils/content";
 import Image from "next/image";
 
 const ShareCards = () => {
   return (
-    <div className="flex-1 flex justify-center items-center pt-20 image-container">
-      <Image src={SHARE_SOCIAL_1} alt="bg" width={205} height={311} className="z-10" />
+    <div className="flex justify-center items-center pt-20 image-container">
+      <Image src={SHARE_SOCIAL_0} alt="bg" width={105} height={311} className="z-10" />
+      <Image src={SHARE_SOCIAL_1} alt="widget" width={105} height={311} className="z-9 " />
+      <Image src={SHARE_SOCIAL_2} alt="widget" width={105} height={311} className="z-8 " />
+      <Image src={SHARE_SOCIAL_3} alt="widget" width={105} height={311} className="z-7 " />
+      <Image src={SHARE_SOCIAL_5} alt="widget" width={105} height={311} className="z-6 " />
     </div>
   );
 };
