77a51649d33dfc58db4f0b75aadf9384d0a65a5a
Nejra <nejra.pribinja@gmail.com>
2024-06-06 12:04:41 +0200

    slider section

--- components/AnalyzeSection/AnalyzeSection.tsx
+++ components/AnalyzeSection/AnalyzeSection.tsx
@@ -1,11 +1,17 @@
 import dynamic from "next/dynamic";
+import Slider from "./Slider";
 const AnalyzeContent = dynamic(() => import("./AnalyzeContent"));
 
 const AnalyzeSection = () => {
   return (
-    <div className="bg-analyzebg min-h-[100vh] flex justify-center items-center " id="home">
-      <AnalyzeContent />
-    </div>
+    <>
+      <div className="bg-analyzebg min-h-[100vh] flex justify-center items-center " id="home">
+        <AnalyzeContent />
+      </div>
+      <div className="bg-analyzebg min-h-[100vh] flex justify-center items-center " id="home">
+        <Slider />
+      </div>
+    </>
   );
 };
 
---
+++ components/AnalyzeSection/Slider.tsx
@@ -0,0 +1,13 @@
+import { SLIDER_TITLE } from "@/utils/content";
+
+const Slider = () => {
+  return (
+    <div className="w-screen flex justify-center items-center">
+      <p className="md:text-[56px] text-black tracking-[-0.02em] leading-tight text-[32px] font-[820]">
+        {SLIDER_TITLE}
+      </p>
+    </div>
+  );
+};
+
+export default Slider;
