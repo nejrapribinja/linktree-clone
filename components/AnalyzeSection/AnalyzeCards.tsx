dd97b5308c86f6fc5c38f2a85322b3ffa6d195e7
Nejra <nejra.pribinja@gmail.com>
2024-06-09 19:45:57 +0200

    cards

--- components/AnalyzeSection/AnalyzeCards.tsx
+++ components/AnalyzeSection/AnalyzeCards.tsx
@@ -1,5 +1,12 @@
+import Image from "next/image";
+import { SHARE_SOCIAL_0 } from "@/utils/content";
+
 const AnalyzeCards = () => {
-  return <div className="flex-1 flex justify-center items-center pt-10"></div>;
+  return (
+    <div className="flex-1 flex justify-center items-center pt-10">
+      <Image src={SHARE_SOCIAL_0} alt="null" width={200} height={200} />
+    </div>
+  );
 };
 
 export default AnalyzeCards;
